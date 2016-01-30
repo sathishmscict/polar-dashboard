63312fc495e16b7ca69a06920be1008db6ac7fbd
Aidan Follestad <drummer.aidan@gmail.com>
2016-01-30 15:29:16 -0600

    Updated Zooper placeholder

--- app/src/main/java/com/afollestad/polar/fragments/ZooperFragment.java
+++ app/src/main/java/com/afollestad/polar/fragments/ZooperFragment.java
@@ -1,5 +1,11 @@
 package com.afollestad.polar.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.afollestad.polar.R;
 import com.afollestad.polar.fragments.base.BasePageFragment;
 
@@ -11,5 +17,11 @@ public class ZooperFragment extends BasePageFragment {
     @Override
     public int getTitle() {
         return R.string.zooper;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_zooper_placeholder, container, false);
     }
 }
