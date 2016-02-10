76771dbc8b8b30c9b4b3334ad0c3ce6053036a09
Aidan Follestad <drummer.aidan@gmail.com>
2016-02-10 16:19:45 -0600

    Progress with Zooper support, Polar packs will be visible to more launchers now

--- app/src/main/java/com/afollestad/polar/fragments/ZooperFragment.java
+++ app/src/main/java/com/afollestad/polar/fragments/ZooperFragment.java
@@ -2,26 +2,142 @@ package com.afollestad.polar.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.util.DialogUtils;
 import com.afollestad.polar.R;
+import com.afollestad.polar.adapters.ZooperAdapter;
+import com.afollestad.polar.config.Config;
 import com.afollestad.polar.fragments.base.BasePageFragment;
+import com.afollestad.polar.ui.MainActivity;
+import com.afollestad.polar.util.TintUtils;
+import com.afollestad.polar.util.Utils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * @author Aidan Follestad (afollestad)
  */
-public class ZooperFragment extends BasePageFragment {
+public class ZooperFragment extends BasePageFragment implements
+        SearchView.OnQueryTextListener, SearchView.OnCloseListener {
+
+    @Bind(android.R.id.list)
+    RecyclerView mRecyclerView;
+    @Bind(android.R.id.empty)
+    TextView mEmpty;
+    @Bind(android.R.id.progress)
+    View mProgress;
+
+    private ZooperAdapter mAdapter;
+    private String mQueryText;
+
+    public ZooperFragment() {
+    }
 
     @Override
     public int getTitle() {
         return R.string.zooper;
     }
 
-    @Nullable
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_zooper_placeholder, container, false);
+        return inflater.inflate(R.layout.fragment_recyclerview, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.zooper, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuItem mSearchItem = menu.findItem(R.id.search);
+        SearchView mSearchView = (SearchView) MenuItemCompat.getActionView(mSearchItem);
+        mSearchView.setQueryHint(getString(R.string.search_widgets));
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setOnCloseListener(this);
+        mSearchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        if (getActivity() != null) {
+            final MainActivity act = (MainActivity) getActivity();
+            TintUtils.themeSearchView(act.getToolbar(), mSearchView, DialogUtils.resolveColor(act, R.attr.tab_icon_color));
+        }
+    }
+
+    private void setListShown(boolean shown) {
+        final View v = getView();
+        if (v != null) {
+            mRecyclerView.setVisibility(shown ?
+                    View.VISIBLE : View.GONE);
+            mProgress.setVisibility(shown ?
+                    View.GONE : View.VISIBLE);
+            mEmpty.setVisibility(shown && mAdapter.getItemCount() == 0 ?
+                    View.VISIBLE : View.GONE);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        if (savedInstanceState == null)
+            setBottomPadding(mRecyclerView, Utils.getNavBarHeight(getActivity()));
+
+        mAdapter = new ZooperAdapter(getActivity());
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(
+                Config.get().gridWidthWallpaper(), StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    // Search
+
+    private final Runnable searchRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mAdapter.filter(mQueryText);
+            setListShown(true);
+        }
+    };
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mQueryText = newText;
+        mRecyclerView.postDelayed(searchRunnable, 400);
+        return false;
+    }
+
+    @Override
+    public boolean onClose() {
+        mRecyclerView.removeCallbacks(searchRunnable);
+        mQueryText = null;
+        mAdapter.filter(null);
+        setListShown(true);
+        return false;
     }
 }
